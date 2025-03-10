2bc2ee2548e059d26699cec6997acb0fa686da39
abin <abinshaju9846@gmail.com>
2025-03-10 16:43:53 +0530

    permisson update

--- libs/users/src/users.module.ts
+++ libs/users/src/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RolesModule } from './roles/roles.module';
+import { PermissionModule } from './permission/permission.module';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
-  imports: [RolesModule],
+  imports: [RolesModule, PermissionModule],
 })
 export class UsersModule {}
